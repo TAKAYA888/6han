c125ec107a79405d44e54f637a5c110f67a3e807
takaya_kanamori <last.takaya888@gmail.com>
2019-11-27 16:09:53 +0900

    テンプレ

---
+++ ActionGame/Game.cs
@@ -0,0 +1,19 @@
+﻿using DxLibDLL;
+
+namespace ActionGame
+{
+    public class Game
+    {
+        public void Init()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw()
+        {
+        }
+    }
+}
---
+++ ActionGame/PlayScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionGame
+{
+    public class PlayScene : Scene
+    {
+        public PlayScene()
+        {
+
+        }
+        public override void Update()
+        {
+            
+        }
+        public override void Draw()
+        {
+            
+        }
+    }
+}
