6565b528757bca6b9e6af6edaf98ef9fc62efa6f
hamadadesu <g019g1376@gmail.com>
2019-11-28 09:57:13 +0900

    imageクラスを変更して、SEクラスを追加しました。

--- ActionGame/Image.cs
+++ ActionGame/Image.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace ActionGame
 {
-    class Image
+    public static class Image
     {
+        //プレイヤーのリソース画像
+
+　　　　//ザコ敵のリソース画像
+
+　　　　//アイテムのリソース画像
+
+        public static void Load()
+        {
+            //プレイヤーのリソース画像
+
+            //ザコ敵のリソース画像
+
+            //アイテムのリソース画像
+
+        }
     }
 }
---
+++ ActionGame/SE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionGame
+{
+    public static class SE
+    {
+        //プレイヤーのリソースSE
+
+        //ザコ敵のリソースSE
+
+        //アイテムのリソースSE
+
+        public static void Load()
+        {
+            //プレイヤーのリソースSE
+
+            //ザコ敵のリソースSE
+
+            //アイテムのリソースSE
+
+        }
+    }
+}
