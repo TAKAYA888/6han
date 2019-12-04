d8b0e9788b732a0702cb64a4ea039557ac673c3e
hamadadesu <g019g1376@gmail.com>
2019-12-04 13:53:06 +0900

    間違えて消してしまったのであげなおしました

--- ActionGame/MyLib.cs
+++ ActionGame/MyLib.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace ActionGame
 {
-    class MyLib
+    public static class MyMath
     {
+        /// <summary> 
+        /// 四角形同士が重なっているか？ 
+        /// </summary> 
+        /// <param name="aLeft">A左端</param> 
+        /// <param name="aTop">A上端</param> 
+        /// <param name="aRight">A右端</param> 
+        /// <param name="aBottom">A下端</param> 
+        /// <param name="bLeft">B左端</param> 
+        /// <param name="bTop">B上端</param> 
+        /// <param name="bRight">B右端</param> 
+        /// <param name="bBottom">B下端</param> 
+        /// <returns>重なっていたらtrue, 重なっていなければfalse</returns> 
+        public static bool RectRectIntersect(
+            float aLeft, float aTop, float aRight, float aBottom,
+            float bLeft, float bTop, float bRight, float bBottom)
+        {
+            return
+                aLeft < bRight &&
+                aRight > bLeft &&
+                aTop < bBottom &&
+                aBottom > bTop;
+        }
+        /// <summary>
+        /// 点から点への角度（ラジアン）を求める。
+        /// </summary>
+        /// <param name="fromX">始点x</param>
+        /// <param name="fromY">始点y</param>
+        /// <param name="toX">終点x</param>
+        /// <param name="toY">終点y</param>
+        /// <returns></returns>
+        public static float PointToPointAngle(float fromX, float fromY, float toX, float toY)
+        {
+            return (float)Math.Atan2(toY - fromY, toX - fromX);
+        }
     }
 }
