62de411a9890c11c9e74f50b2c79d5fefed61880
hinameko <hina0818same@gmail.com>
2019-12-09 18:04:31 +0900

    針を少し修正しました

--- ActionGame/NeedleObject.cs
+++ ActionGame/NeedleObject.cs
@@ -28,7 +28,7 @@ namespace ActionGame
 
         public override void Draw()
         {
-
+            Camera.DrawGraph(x, y, Image.TogeWani02);
         }
 
         public override void OnCollision(ItemObject other)
