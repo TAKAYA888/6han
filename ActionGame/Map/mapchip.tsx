0f34d6028633fa552f5709f9ab470976ae7fe2ec
HanAlter <han390831@gmail.com>
2020-01-16 11:06:50 +0900

    stageのブロックとMAPを更新した

---
+++ ActionGame/Map/mapchip.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="mapchip" tilewidth="60" tileheight="60" tilecount="32" columns="8">
+ <image source="../Image/mapchip.png" width="512" height="256"/>
+</tileset>
